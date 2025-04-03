a608dc9555ac3e6142b731c1c4f7fab6a2a90bba
Miroslav Suchý <msuchy@redhat.com>
2025-04-03 10:03:12 +0200

    update reserved instances

--- get_current_usage.py
+++ get_current_usage.py
@@ -21,7 +21,7 @@ RESERVED_INSTANCES = {
         "t3a.2xlarge": 1,
         "r5a.large": 1,
         "m5a.4xlarge": 1,
-        "c7i.xlarge": 23,
+        "c7i.xlarge": 29,
         "c7g.xlarge": 27,
         "r7a.xlarge": 1,
 }}}
